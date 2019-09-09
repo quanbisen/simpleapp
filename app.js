8301e5b32d90c06d8dea74728b2d1aae42e894ee
lollipop <1769128867@qq.com>
2019-09-09 18:07:39 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-app.get(['/','/index.html'],function(req,res){
-	req.send('Hello World.');
+app.get(['/','/index.html'],function(request,response){
+	response.send('Hello World.');
 }
 );
 app.listen(80);
